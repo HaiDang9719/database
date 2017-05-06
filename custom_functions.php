79b9f8f1dbb9cd31840cf8ef5b9a8375f6452a87
HaiDang9719 <haidanglhp1215@gmail.com>
2017-05-06 14:26:41 +0700

    Add files via upload
    upload test.php and custom_functions.php

---
+++ custom_functions.php
@@ -0,0 +1,202 @@
+<?php
+/*
+UserSpice 4
+An Open Source PHP User Management System
+by the UserSpice Team at http://UserSpice.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Put your custom functions in this file and they will be automatically included.
+
+//bold("<br><br>custom helpers included");
+
+//*** FUNCTIONS TO SUPPORT FLIPPER *****///
+function createTemperatureTable($tablename)
+{
+	 $db = DB::getInstance();
+
+	$value = '';
+	$sql = "CREATE TABLE IF NOT EXISTS $tablename (`Time` DATETIME NOT NULL, `Temperature` DOUBLE NULL, PRIMARY KEY (`Time`)) ENGINE = InnoDB";
+	
+	if (!$db->query($sql, $value)->error())
+	{
+	
+		return true;
+	}
+
+	return false;
+	
+}
+
+function insertTemperatureToDatabase($tablename, $temperature)
+{
+	$db = DB::getInstance();
+
+
+	if(!$db->tableExists($tablename))
+	{
+		createTemperatureTable($tablename);
+	}
+	
+	$updateDate = date('Y-m-d H:i:s');
+	$dataArray = array('Time'=> $updateDate, 'Temperature' =>$temperature);
+	
+	$db->insert($tablename, $dataArray);
+}
+
+
+/**** FUNCTIONS TO SUPPORT IOT EGUN *****/
+
+
+//Retrieve information for all users
+function fetchAllWarehouses() {
+	$db = DB::getInstance();
+	$query = $db->query("SELECT * FROM warehouses");
+	$results = $query->results();
+	return ($results);
+}
+
+//Delete warehouses; array
+function deleteWarehouses($warehouses) {
+	$db = DB::getInstance();
+	$i = 0;
+	foreach($warehouses as $warehousename){
+		$query1 = $db->query("DELETE FROM warehouses WHERE warehousename = ?",array($warehousename));
+		$i++;
+	}
+	return $i;
+}
+
+
+// get warehouseName by MAC addr
+
+function getWarehouseNameByMACAddr($MAC)
+{
+	$db = DB::getInstance();
+	$query1 = $db->query("SELECT warehouseName FROM warehouses WHERE MAC = ?",array($MAC));
+	$result1 = $query1->results();
+	foreach($result1 as $a){ $num = $a->warehouseName;echo $num;}
+	return $num;
+	//echo $query->results();
+
+}
+
+function getMACaddrByWarehouseName($Name)
+{
+
+	 $db = DB::getInstance();
+
+	
+	$sql = "SELECT * FROM warehouses WHERE warehouseName = ? ";
+	$query = $db ->get('warehouses',['warehouseName','=',$Name],false);
+
+	return $query->results();
+}
+
+// create RFID Table to Database
+function createRFIDTable($tablename)
+{
+         $db = DB::getInstance();
+
+        $value = '';
+        $sql = "CREATE TABLE IF NOT EXISTS $tablename (
+  `Time` DATETIME NOT NULL,
+  `Pressure` DOUBLE NULL,
+  `Voltage` DOUBLE NULL,
+  `Current` DOUBLE NULL,
+  PRIMARY KEY (`Time`),
+  UNIQUE INDEX `Time_UNIQUE` (`Time` ASC))
+ENGINE = InnoDB";
+
+        if (!$db->query($sql, $value)->error())
+        {
+
+                return true;
+        }
+
+	return false;
+
+}
+
+
+// Insert data to RFID Table in Database
+function insertRFIDTableToDatabase($RFID,$pressure,$voltage,$currentI)
+{
+	$db = DB::getInstance();
+	if(!$db->tableExists($RFID))
+	{
+		createRFIDTable($RFID);
+	}
+	$updateDate = date('Y-m-d H:i:s');
+	$dataArray = array('Time'=> $updateDate,'Pressure' =>$pressure,'Voltage'=>$voltage,'Current'=>$currentI);
+	
+	$db->insert($RFID, $dataArray);
+	
+
+}
+//Find RFID Table base on RFID
+function findRFID($RFID)
+{
+	$db = DB::getInstance();
+	$query = $db->query("SELECT * FROM $RFID");
+        return $query->results();
+}
+
+
+//Find RFID Table base on StationID
+function findRFIDwithStationID($StationID)
+{
+	$db = DB::getInstance();
+	$query1 = $db->query("SELECT RFID FROM stations WHERE id=?",array($StationID));
+	//$query1 = $db ->get('stations',['id','=',$StationID],false);
+	$results=$query1->results();
+	foreach($results as $a){ $rfid=$a->RFID; return(findRFID($rfid));}
+}
+
+//Find StationID 
+function findStationID($sdcsAddr,$sdcsCH,$warehouseName)
+{
+	$db = DB::getInstance();
+	$query1 = $db->query("SELECT id FROM stations WHERE sdcsAddr = ? AND sdcsCH = ? AND warehouseName = ?  ",[$sdcsAddr,$sdcsCH,$warehouseName]);
+        $result1 = $query1->results();
+        foreach($result1 as $a){ $num = $a->id;echo $num;}
+
+}
+//Insert RFID to rfids Table
+function insertRFIDtorfidsTable($RFID,$pressure,$voltage,$currentI){
+	$db = DB::getInstance();
+	$sql = $db->query("SELECT count(*) as total  FROM rfids WHERE RFID = ?",array($RFID));
+	$result1 = $sql->results();
+	foreach($result1 as $a){$num = $a->total;}
+	if ($num ==0){
+	$field = array('RFID'=>$RFID);
+        $db ->insert('rfids',$field);
+	insertRFIDTableToDatabase($RFID,$pressure,$voltage,$currentI);
+	}
+
+
+}
+
+//Override RFID in stations Table
+function insertRFIDtoStationTable($RFID,$sdcsAddr,$sdcsCH,$warehouseName)
+{
+	$db = DB::getInstance();
+	$query1 = $db->query("SELECT id FROM stations WHERE sdcsAddr = ? AND sdcsCH = ? AND warehouseName = ?  ",[$sdcsAddr,$sdcsCH,$warehouseName]);
+	$result1 = $query1->results();
+	foreach($result1 as $a){ $num = $a->id;}
+	$field = array('RFID'=>$RFID,'sdcsAddr'=>$sdcsAddr,'sdcsCH'=>$sdcsCH,'warehouseName'=>$warehouseName);
+	$db->update('stations',$num,$field);
+
+}
---
+++ test.php
@@ -0,0 +1,90 @@
+<?php 
+require_once 'users/init.php';
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+
+// $MAC = getMACaddrByWarehouseName("USA1");
+
+// foreach ($MAC as $M) {
+//     echo $M->MAC;
+// }
+// $RFID = "100hg";
+// $pressure = 100;
+// $voltage = 120;
+// createRFIDTable("abcde");
+// createTemperatureTable("abcd");
+// $currentI = 100;
+// insertRFIDTableToDatabase("abcd","100","20","30");
+// $k = findRFID("1a");
+
+// foreach ($k as $M) {
+//     echo $M->Pressure;
+// }
+
+// $fk = findRFIDwithStationID("2");
+// foreach ($fk as $M) {
+//     echo $M->Voltage;
+//}
+//insertRFIDtorfidsTable("10p");
+//insertRFIDtoStationTable("8391029","5","6","ok");
+
+//createRFIDTable('R123456'); 
+//insertRFIDtorfidsTable('R456345',190,200,10);
+//findStationID("5","6","ok");
+$json = '{"e":0,"i":{"r": 0,"d":   0},"g":{"a":[1    ],"p":[ 5000            ]},"p":[{"a":1,"d":[{"p":[ 8344      ],"r":[ 42, 14, 63,126]},{"p":[ 3855      ],"r":[250,158, 77,126]},{"p":[   60      ],"r":[  0,  0,  0,  0]},{"p":[   61      ],"r":[  0,  0,  0,  0]},{"p":[   62      ],"r":[  0,  0,  0,  0]},{"p":[   58      ],"r":[  0,  0,  0,  0]}]},{"a":2,"d":[{"p":[   30      ],"r":[ 22,108, 33,158]},{"p":[   37      ],"r":[ 86,160,  3,158]},{"p":[ 2514      ],"r":[166, 61, 53,158]},{"p":[ 2493      ],"r":[214,246, 49,158]},{"p":[ 2495      ],"r":[ 86,160, 17,158]},{"p":[ 2975      ],"r":[102,152,255,157]}]}],"m":[  0, 26,182,  3,  4, 25]}';
+$result = json_decode ($json,true);
+$mac_count = 0;
+$WHMAC = "";
+while ($mac_count <= 5) {
+        $WHMAC .= $result['m'][$mac_count];
+        $mac_count++;
+}
+$i=0;
+$RFID = "R";
+$rfid_count = 1;
+$sdcsAddr_count = 0;
+$sdcsCH = 0;
+$numberOfsdcsAddr = count($result['p']);
+while (($sdcsAddr_count <=$numberOfsdcsAddr-1) and ($numberOfsdcsAddr >0)){
+	
+while ($rfid_count <=6){
+	$sdcsAddr = $result['p'][$sdcsAddr_count]['a'];
+	
+	
+	while ( $i<= 3) {
+	# code...
+
+		$RFID .= $result['p'][$sdcsAddr_count]['d'][$rfid_count-1]['r'][$i];
+		$i++;
+	}
+	$i=0;
+	if ($RFID != 'R0000') {
+		$sdcsCH = $rfid_count;
+		//echo $sdcsCH."---sdcsCH------";
+		//echo $sdcsAddr."---sdcsAddr------";
+		$pressure = $result['p'][$sdcsAddr_count]['d'][$rfid_count-1]['p'][0];
+		//echo $result['p'][$sdcsAddr_count]['d'][$rfid_count-1]['p'][0]."----Pressure------";
+		//echo $RFID."---RFID"."\n";
+		//echo $WHMAC;
+		$warehouseName=getWarehouseNameByMACAddr($WHMAC);
+		//echo $warehouseName;
+		//echo $sdcsAddr;
+		//echo $sdcsCH;
+		insertRFIDtorfidsTable($RFID,$pressure,0,0);
+		insertRFIDtoStationTable($RFID,$sdcsAddr,$sdcsCH,$warehouseName);
+
+		
+	}
+	$RFID = "R";
+	
+	$rfid_count ++;
+}
+	$rfid_count = 1;
+	$RFID = "R";
+$sdcsAddr_count++;
+}
+
+
+//echo $WHMAC."----WHMac";
+
+?>
